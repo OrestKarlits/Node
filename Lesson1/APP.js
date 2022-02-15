a3d44c4017cfccd5f909e8960c4a497acfd53b2e
OrestKarlits <orestkarlits@gmail.com>
2022-02-15 02:04:00 +0200

    lesson1

---
+++ Lesson1/APP.js
@@ -0,0 +1,22 @@
+const start = require('path');
+const add = require('fs');
+
+// add.mkdir(start.join(__dirname, 'main'), (eror) => {
+//     if (eror) {
+//         console.log(eror);
+//     }
+// })
+
+
+// add.mkdir(start.join(__dirname, 'main', 'online'), {recursive: true},(eror) => {
+//     if (eror) {
+//         console.log(eror);
+//     }
+// })
+
+
+// add.mkdir(start.join(__dirname, 'main', 'inPerson'), {recursive: true},(eror) => {
+//     if (eror) {
+//         console.log(eror);
+//     }
+// })
